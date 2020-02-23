70b1d67aeaf6d1df018f48c82a7c69d6279d0bb9
Zrbana <1637680639@qq.com>
2020-02-23 22:15:54 +0800

    solution of problems

---
+++ PracticeProject/src/java7/daily/learning/Test.java
@@ -0,0 +1,108 @@
+package java7.daily.learning;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Test {
+
+}
+
+//在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，
+// 每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+class Solution {
+    public boolean Find(int [][] array,int target) {
+        boolean found = false;
+        int lie = array[0].length;
+        int hang = array.length;
+        int column = lie -1;
+        int row =0;
+        while(row<hang &&column>=0){
+            int value = array[row][column];
+            if(target>value){
+                row++;
+            }else if(value>target){
+                column--;
+            }else{
+                found = true;
+                break;
+            }
+
+        }
+        return found;
+    }
+}
+
+
+/**
+ * 将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+ */
+class Solution_01 {
+    public String replaceSpace(StringBuffer str) {
+        int spacenum = 0;//spacenum为计算空格数
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' ')
+                spacenum++;
+        }
+        int indexold = str.length()-1; //indexold为为替换前的str下标
+        int newlength = str.length() + spacenum*2;//计算空格转换成%20之后的str长度
+        int indexnew = newlength-1;//indexold为为把空格替换为%20后的str下标
+        str.setLength(newlength);//使str的长度扩大到转换成%20之后的长度,防止下标越界
+        for(;indexold>=0 && indexold<newlength;--indexold){
+            if(str.charAt(indexold) == ' '){  //
+                str.setCharAt(indexnew--, '0');
+                str.setCharAt(indexnew--, '2');
+                str.setCharAt(indexnew--, '%');
+            }else{
+                str.setCharAt(indexnew--, str.charAt(indexold));
+            }
+        }
+        return str.toString();
+    }
+}
+
+//输入一个链表，按链表从尾到头的顺序返回一个ArrayList。
+class Solution_03{
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        Stack<Integer> stack = new Stack<>();
+        while (listNode != null) {
+            stack.push(listNode.val);
+            listNode = listNode.next;
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            list.add(stack.pop());
+        }
+        return list;
+    }
+}
+class ListNode {
+      int val;
+      ListNode next = null;
+      ListNode(int val) {
+          this.val = val;
+     }
+    }
+
+
+
+//用两个栈实现队列
+class Solution_04 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+        int first=stack2.pop();
+        while(!stack2.isEmpty()){
+            stack1.push(stack2.pop());
+        }
+        return first;
+    }
+}
