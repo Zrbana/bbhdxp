d5f29e6e09e5dd07a10dfa1a5e1abbe2ed33d98d
Zrbana <1637680639@qq.com>
2020-04-16 22:59:22 +0800

    mybaits

---
+++ mybatis_Table/src/main/java/domain/User.java
@@ -0,0 +1,76 @@
+package domain;
+
+import java.util.Date;
+import java.util.List;
+
+public class User {
+    //实现该接口是为了更好的存储和运输
+
+    private int id;
+    private String username;
+    private Date birthday;
+    private String sex;
+    private String address;
+    //主表与从表一对多的关系，主表的实体类需要包含从表的实体类的集合引用
+    private List<Account> accounts;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", birthday=" + birthday +
+                ", sex='" + sex + '\'' +
+                ", address='" + address + '\'' +
+                ", accounts=" + accounts +
+                '}';
+    }
+}
